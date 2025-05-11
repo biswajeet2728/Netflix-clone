bf72bbbafd7829f58b42540fdd67a36ecafffe80
biswajeet2728 <sanyalbiswajeet03@gmail.com>
2025-05-12 03:54:32 +0530

    Update n.js

--- n.js
+++ n.js
@@ -4,6 +4,11 @@ let inp3 = document.getElementById("input");
 let bu = document.getElementById("b");
 let bu2 = document.getElementById("b2");
 let bu3 = document.getElementById("button");
+let change = document.getElementById("slide");
+let fm = document.querySelectorAll(".f");
+let Sm = document.querySelectorAll(".S");
+let show = true;
+
 const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 
@@ -18,6 +23,20 @@ function check(){
     
     
 };
+change.addEventListener("click", ()=>{
+    if(show){
+        Sm.forEach((i) => i.classList.remove("S"));
+        fm.forEach((i) => i.style.display = "none");
+        change.innerText = "For more Sign up"
+        show = false;
+    }
+    else{
+        Sm.forEach((i) => i.classList.add("S"));
+        fm.forEach((i) => i.style.display="");
+        show = true;
+    }
+
+});
 
 bu.addEventListener("click" , check);
 bu2.addEventListener("click" , check);
